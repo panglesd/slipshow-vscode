9b3f4fbfb7b58b7c2c24febc55787e067b35cc16
Paul-Elliot <peada@free.fr>
2024-08-14 10:06:53 +0200

    Cleanup

--- src/bin/main.ml
+++ src/bin/main.ml
@@ -1,62 +1,48 @@
-(* type _ Effect.t += GetImg : string -> string option Effect.t *)
-
 let resolve_images document : string -> Slipshow.asset =
   let open Vscode in
   fun s ->
-    if
-      Astring.String.is_infix ~affix:"://" s
-      || String.starts_with ~prefix:"//" s
-    then Slipshow.Remote s
-    else
-      let oc = Window.createOutputChannel ~name:"Slipshow" in
-      let log value = OutputChannel.appendLine oc ~value in
-      let s = Fpath.v s in
-      let s =
-        if Fpath.is_abs s then s
-        else
-          let open Vscode in
-          let root =
-            TextDocument.fileName document |> Fpath.v |> Fpath.parent
-          in
-          (* let _s = "/tmp/vscodetest/old_pipe_copy.png" in *)
-          (* let r = *)
-          (*   Vscode.Workspace.rootPath () |> function *)
-          (*   | Some s -> Fpath.v s *)
-          (*   | None -> Fpath.v "none" *)
-          (* in *)
-          Fpath.( // ) root s
-      in
-      let content =
-        let content =
-          Node.Fs.readFileSync @@ Fpath.to_string s
-          |> Node.Buffer.toBase64 |> Base64.decode_exn
+    try
+      if
+        Astring.String.is_infix ~affix:"://" s
+        || String.starts_with ~prefix:"//" s
+      then Slipshow.Remote s
+      else
+        let s = Fpath.v s in
+        let s =
+          if Fpath.is_abs s then s
+          else
+            let open Vscode in
+            let root =
+              TextDocument.fileName document |> Fpath.v |> Fpath.parent
+            in
+            Fpath.( // ) root s
         in
-        (* let content = Effect.perform (GetImg (Fpath.to_string s)) in *)
-        log (Base64.encode content |> Result.get_ok);
-        (* print_endline content; *)
-        content
-      in
-
-      (* let content = "rien" in *)
-      (* let () = print_endline content in *)
-      let mime_of_ext = function
-        | "apng" ->
-            Some "image/apng" (* Animated Portable Network Graphics (APNG) *)
-        | "avif" -> Some "image/avif" (*  AV1 Image File Format (AVIF) *)
-        | "gif" -> Some "image/gif" (* Graphics Interchange Format (GIF) *)
-        | "jpeg" ->
-            Some "image/jpeg" (* Joint Photographic Expert Group image (JPEG) *)
-        | "png" -> Some "image/png" (* Portable Network Graphics (PNG) *)
-        | "svg+xml" -> Some "image/svg+xml" (* Scalable Vector Graphics (SVG) *)
-        | "webp" -> Some "image/webp" (* Web Picture format (WEBP) *)
-        | _ -> None
-      in
-      let mime_type = mime_of_ext (Fpath.get_ext (* Fpath.v *) s) in
-      Local { mime_type; content }
-(* Slipshow.Remote content *)
+        let content =
+          let content =
+            Node.Fs.readFileSync @@ Fpath.to_string s
+            |> Node.Buffer.toBase64 |> Base64.decode_exn
+          in
+          content
+        in
+        let mime_of_ext = function
+          | "apng" ->
+              Some "image/apng" (* Animated Portable Network Graphics (APNG) *)
+          | "avif" -> Some "image/avif" (*  AV1 Image File Format (AVIF) *)
+          | "gif" -> Some "image/gif" (* Graphics Interchange Format (GIF) *)
+          | "jpeg" ->
+              Some
+                "image/jpeg" (* Joint Photographic Expert Group image (JPEG) *)
+          | "png" -> Some "image/png" (* Portable Network Graphics (PNG) *)
+          | "svg+xml" ->
+              Some "image/svg+xml" (* Scalable Vector Graphics (SVG) *)
+          | "webp" -> Some "image/webp" (* Web Picture format (WEBP) *)
+          | _ -> None
+        in
+        let mime_type = mime_of_ext (Fpath.get_ext s) in
+        Local { mime_type; content }
+    with _ -> Remote s
 
 let slipshow_callback ~args:_ =
-  print_endline "yoooOOOOOOOOOOOOOOO";
   let open Vscode in
   match Window.activeTextEditor () with
   | None -> ()
@@ -115,29 +101,12 @@ let preview_callback extension ~args:_ =
       let document = TextEditor.document editor in
       let the_uri = TextDocument.uri document in
       let text = TextDocument.getText document () in
-      (* let path = *)
-      (*   TextDocument.fileName document *)
-      (*   |> Fpath.v |> Fpath.set_ext "html" |> Fpath.to_string *)
-      (* in *)
       let wb = WebviewPanel.webview panel in
-
-      (* let disposable = *)
-      (*   Window.onDidChangeActiveTextEditor () *)
-      (*     ~listener:(fun _td -> *)
-      (*       let _ = *)
-      (*         Vscode.Window.showInformationMessage *)
-      (*           ~message:"Changed Text was called" () *)
-      (*       in *)
-      (*       ()) *)
-      (*     () *)
-      (* in *)
       let update_content text =
         let resolve_images = resolve_images document in
         let delayed = Slipshow.delayed ~resolve_images text in
         let text = Slipshow.delayed_to_string delayed in
-        WebView.postMessage
-          (WebviewPanel.webview panel)
-          (Ojs.string_to_js (* slipshow_content *) text)
+        WebView.postMessage (WebviewPanel.webview panel) (Ojs.string_to_js text)
       in
 
       let listener td =
@@ -146,22 +115,11 @@ let preview_callback extension ~args:_ =
         let is_good = Uri.equal uri the_uri in
         let document = TextEditor.document editor in
         let text = TextDocument.getText document () in
-        (* let slipshow_content = Slipshow.convert text in *)
-        (* let _ = WebView.set_html wb html in *)
         let _ = if is_good then update_content text else Promise.return false in
         ()
       in
       let disposable = Workspace.onDidChangeTextDocument ~listener () in
       Vscode.ExtensionContext.subscribe extension ~disposable;
-      let _slipshow_content = Slipshow.convert text in
-      (* let _message = *)
-      (*   let _ = Node.Fs.writeFile path ~content:slipshow_content in *)
-      (*   () *)
-      (*   (\* match Bos.OS.File.write path slipshow_content with *\) *)
-      (*   (\* | Error (`Msg m) -> Format.sprintf "Error: %s" m *\) *)
-      (*   (\* | Ok () -> "OK" *\) *)
-      (* in *)
-      (* (\* let _ = Vscode.Window.showInformationMessage ~message () in *\) *)
       let options = WebviewOptions.create ~enableScripts:true () in
       let () = WebView.set_options wb options in
       let html =
