8b57eac7d6c1572ed92add44b22b64dbc491b08f
Paul-Elliot <peada@free.fr>
2026-06-04 15:28:37 +0200

    Replace native extension with lsp client

--- src/bin/main.ml
+++ src/bin/main.ml
@@ -1,190 +1,30 @@
-let read_file document : Slipshow.file_reader =
- fun s ->
-  try
-    let s =
-      if Fpath.is_abs s then s
-      else
-        let open Vscode in
-        let root = TextDocument.fileName document |> Fpath.v |> Fpath.parent in
-        Fpath.( // ) root s
-    in
-    let content =
-      let content =
-        Node.Fs.readFileSync @@ Fpath.to_string s
-        |> Node.Buffer.toBase64 |> Base64.decode_exn
-      in
-      content
-    in
-    Ok (Some content)
-  with _ -> Ok None
-
-let slipshow_callback ~args:_ =
-  let open Vscode in
-  match Window.activeTextEditor () with
-  | None -> Ojs.unit_to_js ()
-  | Some editor ->
-      let document = TextEditor.document editor in
-      let text = TextDocument.getText document () in
-      let path =
-        TextDocument.fileName document
-        |> Fpath.v |> Fpath.set_ext "html" |> Fpath.to_string
-      in
-      let read_file = read_file document in
-      let _ =
-        let open Promise.Syntax in
-        (* TODO: handle warnings *)
-        let+ slipshow_content, _warnings =
-          (* Effect.Deep.try_with *)
-          (*   (fun () ->  *)
-          let entry_point = Fpath.v "-" in
-          let read_file f =
-            if Fpath.equal entry_point f then Ok (Some text) else read_file f
-          in
-          Promise.return
-          @@ Slipshow.convert ~has_speaker_view:true ~read_file entry_point
-          (* ) *)
-          (* () *)
-          (* { *)
-          (*   effc = *)
-          (*     (fun (type a) (eff : a Effect.t) -> *)
-          (*       match eff with *)
-          (*       | GetImg path -> *)
-          (*           Some *)
-          (*             (fun (k : (a, _) Effect.Deep.continuation) -> *)
-          (*               let* content = Node.Fs.readFile path in *)
-          (*               Effect.Deep.continue k *)
-          (*                 (Some *)
-          (*                    content *)
-          (*                    (\* Slipshow.Remote ("YOOOO" ^ Fpath.to_string path) *\))) *)
-          (*       | _ -> None); *)
-          (* } *)
-        in
-        let _message =
-          let _ = Node.Fs.writeFile path ~content:slipshow_content in
-          ()
-          (* match Bos.OS.File.write path slipshow_content with *)
-          (* | Error (`Msg m) -> Format.sprintf "Error: %s" m *)
-          (* | Ok () -> "OK" *)
-        in
-        (* let _ = Vscode.Window.showInformationMessage ~message () in *)
-        ()
-      in
-      Ojs.unit_to_js ()
-
-let preview_callback extension ~args:_ =
-  let open Vscode in
-  match Window.activeTextEditor () with
-  | None -> Ojs.unit_to_js ()
-  | Some editor ->
-      let panel =
-        Window.createWebviewPanel ~viewType:"slipshowPanel"
-          ~title:"Slipshow preview" ~showOptions:ViewColumn.Two
-      in
-      let document = TextEditor.document editor in
-      let the_uri = TextDocument.uri document in
-      let text = TextDocument.getText document () in
-      let wb = WebviewPanel.webview panel in
-      let update_content =
-        let timer = ref None in
-        fun text ->
-          let go () =
-            timer := None;
-            let read_file = read_file document in
-            let entry_point = Fpath.v "-" in
-            let read_file f =
-              if Fpath.equal entry_point f then Ok (Some text) else read_file f
-            in
-            let delayed, warnings =
-              Slipshow.delayed ~has_speaker_view:true ~read_file entry_point
-            in
-            let warnings =
-              List.map
-                (Format.asprintf "%a@.@."
-                   (Grace_ansi_renderer.pp_diagnostic ?config:None
-                      ~code_to_string:Diagnosis.to_code))
-                warnings
-            in
-            let warnings = List.map (Ansi.process (Ansi.create ())) warnings in
-            let warnings = String.concat "" warnings in
-            let text = Slipshow.delayed_to_string (delayed, warnings) in
-            WebView.postMessage
-              (WebviewPanel.webview panel)
-              (Ojs.string_to_js text)
-          in
-          let () =
-            match !timer with Some id -> Brr.G.stop_timer id | None -> ()
-          in
-          let i =
-            Brr.G.set_timeout ~ms:500 (fun _ ->
-                let _ = go () in
-                ())
-          in
-          timer := Some i
-      in
-
-      let listener td =
-        let contentChanges = TextDocumentChangeEvent.contentChanges td in
-        let document = TextDocumentChangeEvent.document td in
-        let uri = TextDocument.uri document in
-        let is_good =
-          Uri.equal uri the_uri && (not @@ List.is_empty contentChanges)
-        in
-        let document = TextEditor.document editor in
-        let text = TextDocument.getText document () in
-        let () = if is_good then update_content text in
-        ()
-      in
-      let disposable = Workspace.onDidChangeTextDocument ~listener () in
-      Vscode.ExtensionContext.subscribe extension ~disposable;
-      let options = WebviewOptions.create ~enableScripts:true () in
-      let () = WebView.set_options wb options in
-      let html =
-        Format.sprintf
-          {|<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-           <title>Slipshow</title>
-           <style>
-</style>
-</head>
-           <body>
-           <div id="iframes" style="position:absolute;inset:0;">
-           </div>
-           <pre id="warnings-slipshow" class="hide-warnings"></pre>
-           <div id="warnings-slipshow-show">⚠️</div>
-           <style>%s</style>
-           <style>%s</style>
-           <script>%s</script>
-</body>
-</html>
-         |}
-          (* Assets.(read Index_js) *)
-          Ansi.css Server_assets.Style.v
-          [%blob "../src-panel/vscode_previewer.bc.js"]
-      in
-      let _ = WebView.set_html wb html in
-      let _ = update_content text in
-      Ojs.unit_to_js ()
+let client_state = ref None
 
 let activate context =
-  let ss_disposable =
-    Vscode.Commands.registerCommand ~command:"slipshow.slipshow"
-      ~callback:slipshow_callback
+  let open Vscode_languageclient in
+  let serverOptions =
+    ServerOptions.create ~args:[ "lsp" ] ~command:"slipshow" ()
   in
-  let prs_disposable =
-    Vscode.Commands.registerCommand ~command:"slipshow.preview"
-      ~callback:(preview_callback context)
+  let documentSelector =
+    [| DocumentSelector.language ~scheme:"file" "slipshow" |]
   in
-  Vscode.ExtensionContext.subscribe context ~disposable:ss_disposable;
-  Vscode.ExtensionContext.subscribe context ~disposable:prs_disposable
+  let clientOptions = ClientOptions.create ~documentSelector () in
+  let id = "slipshow" and name = "Slipshow" in
+  let client = LanguageClient.make ~id ~name ~serverOptions ~clientOptions () in
+  client_state := Some client;
+  let open Promise.Syntax in
+  let+ () = LanguageClient.start client in
+  Control.register client context
+
+let deactivate _ =
+  match !client_state with
+  | Some client -> Vscode_languageclient.LanguageClient.stop client
+  | None -> Promise.return ()
 
 let () =
   let open Js_of_ocaml.Js in
   export "activate" (wrap_callback activate)
 
-(* I had to update vscode-ocaml-platform to support latest ocaml, but its API
-   has changed, and I was forced to change the callback from returning unit to
-   returning Ojs.t. I have implemented something, but did not test. If something
-   breaks, look there, me from the future! *)
+let () =
+  let open Js_of_ocaml.Js in
+  export "deactivate" (wrap_callback deactivate)
